8b336245ec0f396090871d70c5a44ada734fe559
Ibrahim <ibraguim.karsamov@gmail.com>
2023-03-10 08:06:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
+
+function substract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber
+}
