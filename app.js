be020666f921391155e55352c16800cfd28c35f6
Ibrahim <ibraguim.karsamov@gmail.com>
2023-03-11 16:19:10 +0100

    script

--- app.js
+++ app.js
@@ -1,15 +1,72 @@
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber
-}
+const previousInput = document.querySelector('#previous-input');
+const currentInput = document.querySelector('#current-input');
 
-function substract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber
-}
+let previousNumber = '';
+let currentNumber = '';
+let operator = '';
 
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber
-}
+document.querySelector('#equalBtn').addEventListener('click', calculate);
 
-function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber
-}
+function calculate() {
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+    
+    switch (operator) {
+        case '+':
+            previousNumber = previousNumber + currentNumber;
+            currentInput.textContent = previousNumber;
+            break;
+        case '-':
+            previousNumber = previousNumber - currentNumber;
+            currentInput.textContent = previousNumber;
+            break;
+        case 'x':
+            previousNumber = previousNumber * currentNumber;
+            currentInput.textContent = previousNumber;
+            break;
+        case '÷':
+            previousNumber = previousNumber / currentNumber;
+            currentInput.textContent = previousNumber;
+            break;
+    };
+    previousInput.textContent = '';
+};
+
+document.querySelectorAll('.digitBtn').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (currentNumber.length <= 11) {
+            currentNumber += e.target.textContent;
+            currentInput.textContent = currentNumber;
+        };
+    });
+});
+
+document.querySelectorAll('.operatorBtn').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        operator = e.target.textContent;
+        previousNumber = currentNumber;
+        previousInput.textContent = `${previousNumber} ${operator}`;
+        currentNumber = '';
+        currentInput.textContent = '';
+    });
+});
+
+document.querySelector('#clearBtn').addEventListener('click', () => {
+    previousInput.textContent = '';
+    currentInput.textContent = '';
+    previousNumber = '';
+    currentNumber = '';
+    operator = '';
+});
+
+document.querySelector('#pointBtn').addEventListener('click', () => {
+    if (!currentNumber.includes('.')) {
+        currentNumber += '.';
+        currentInput.textContent = currentNumber;
+    };
+});
+
+document.querySelector('#deleteBtn').addEventListener('click', () => {
+    currentNumber = currentNumber.slice(0, -1);
+    currentInput.textContent = currentNumber;
+});
